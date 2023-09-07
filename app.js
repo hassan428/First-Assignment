04e01078ab4ea8a999a2661897bd84ba03b3f07a
has-123-san <hassanhanif2345@gmail.com>
2023-09-07 23:40:26 +0500

    Assignment Complete

---
+++ app.js
@@ -0,0 +1,224 @@
+
+// Chapter 1 (Alerts)  
+
+// Question 1
+
+alert("Hassan")
+alert("Hanif")
+alert("abc123@gmail.com")
+alert("0300-0000-000")
+alert("Password")
+
+
+// Question 2
+
+alert("You're learning JavaScript!")
+
+
+// Question 3
+
+alert("Welcome Users")
+
+
+// Chapter 1 Completed
+
+
+// Chapter 2 (Variables for string)
+
+
+// Question 1
+
+var firstWeb;
+
+
+// Question 2
+
+var html_developer = "I am working JavaScript"
+
+
+// Question 3
+
+var teamName = "Web & Mob_App Developer"
+
+alert(teamName)
+
+
+// Question 4
+
+var bestMan = "Charlie";
+
+var bestMan = "Babar, Rizwan, Shaheen, Rauf & Naseem"
+
+alert(bestMan)
+
+
+// Chapter 2 Completed
+
+
+// Chapter 3 (Variables for numbers)
+
+
+// Question 1
+
+var caseQty;  
+
+
+// Question 2
+
+var case_Qty = 144
+
+case_Qty = 150  
+
+
+// Question 3
+
+var number = 9;  
+
+var results = number + 16
+
+
+
+// Question 4
+
+var twoNumbers;
+
+twoNumbers = 5 + 7
+
+
+// Question 5
+
+var merchTotal = 250;
+var shippingCharge = 40;
+var orderTotal = merchTotal + shippingCharge;
+
+document.write(orderTotal)
+
+
+// Question 6
+
+var num_ber = 50    
+
+var num_bers = 150
+
+var result = num_ber + num_bers
+
+document.write(result)
+
+// Chapter 3 Completed
+
+// Chapter 4 (Variable names Legal and Illegal)
+
+
+// Question 1
+
+var product_cost = 3.45;
+
+
+// Question 2
+
+var nameOfBand; "It's camelCase Variable"
+
+
+// Question 3
+
+var myNumber = 100
+
+
+// Question 4
+
+var firstAndLastName = "Hassan Hanif"
+
+
+// Question 5
+
+// Legal Variable
+
+var legal_variable;
+
+var variable09;
+
+var legalVariable;
+
+var $dollar;
+
+var MAY_BE;
+
+// Illegal Variable
+
+
+//  123var:
+
+//  il-legal;
+
+//  @illegal;
+
+//  var;
+
+//  illegal variable;
+
+
+
+// Chapter 4 Completed
+
+
+// Chapter 5 (Math Expression I)
+
+
+// Question 1
+
+var name = "Modulo operator"
+
+var symbol = "%"
+
+
+// Question 2
+
+var num = 20 % 6 
+
+console.log(num)
+
+// Answer is 2
+
+
+
+// Question 3
+
+var largeNum = 1000 * 2000
+
+console.log(largeNum)
+
+// Answer is 2000000
+
+
+
+// Question 4
+
+var var1 = 15
+
+var var2 = 5
+
+var res = var1 - var2
+
+// Answer is 10
+
+
+
+// Question 5
+
+var moddulos = 50 % 7
+
+// Answer is 1
+  
+
+
+// Question 6
+
+var num1 = 5
+
+var num2 = 6
+
+var result3 = num1 * num2
+
+alert(result3)
+
+
